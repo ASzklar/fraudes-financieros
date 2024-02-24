ae4f19315685fffc34cda2c39d598f3b76a55525
ASzklar <111814288+ASzklar@users.noreply.github.com>
2024-02-23 21:00:44 -0300

    Corrigiendo el script

--- NC.py
+++ NC.py
@@ -1,9 +1,35 @@
 import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+import io
 
-df = pd.read_csv('https://drive.google.com/file/d/1YvSbFLTblzd7kfYxW6IMNT9JIM_8WHLv/view?usp=drive_link')
+# ID del archivo CSV
+file_id = '1YvSbFLTblzd7kfYxW6IMNT9JIM_8WHLv'
 
+# Clave de API (pega tu clave aquí)
+api_key = "AIzaSyBxZk18sBSo0GMrzJgjzHV1FsCSYLrAji4"
+
+# Servicio Drive
+service = build('drive', 'v3', developerKey=api_key)
+
+# Descargar el archivo CSV a un buffer
+request = service.files().get_media(fileId=file_id)
+file_buffer = request.execute()
+
+# Convert the bytes object to a string
+text_buffer = file_buffer.decode("utf-8")
+
+# Create an in-memory StringIO object
+string_io = io.StringIO(text_buffer)
+
+# Read the CSV with pandas
+df = pd.read_csv(string_io)
+
+print(df.head())
+
+print('Hello World')
 
 st.title("Probando Streamlit :hand:")
 st.text("No Country Proyecto detección de fraudes")
@@ -22,6 +48,5 @@ st.write("***Fraudes***")
 #ax.set_ylabel('Cantidad')
 #st.pyplot(fig)
 
-#st.dataframe(df.head())
 
 #@st.cache
